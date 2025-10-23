27764c3fc4a98dca4385811cec66a030d7a546bf
Özge Yıldırım <o.yildirimm2@gmail.com>
2025-10-23 15:06:30 +0300

    first commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,15 @@
-import { Component, signal } from '@angular/core';
+import { Component} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from "./components/header/header";
+import { Sidebar } from "./components/sidebar/sidebar";
+import { CreateCustomer } from "./pages/create-customer/create-customer";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Header, Sidebar, CreateCustomer],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
-export class App {
-  protected readonly title = signal('telcocrm-fe');
+export class AppComponent {
+  
 }
---
+++ src/app/pages/create-customer/create-customer.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-customer',
+  imports: [],
+  templateUrl: './create-customer.html',
+  styleUrl: './create-customer.scss',
+})
+export class CreateCustomer {
+
+}
